41141812ea9bf8c68c7bda56f5fd448e1c812947
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-08-11 20:27:09 +0600

    More problems solved

--- PATTERN PRINTING/hill_pattern.java
+++ PATTERN PRINTING/hill_pattern.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 
-public class left_sided_triangle {
+public class hill_pattern {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             System.out.print("Enter pattern size: ");
             int n = input.nextInt();
+
             for (int i = 1; i <= n; i++) {
-                for (int j = 1; j < i; j++) {
+                for (int j = i; j < n; j++) {
                     System.out.print("  ");
                 }
-                for (int j = i; j <= n; j++) {
+                for (int j = 1; j <= i; j++) {
                     System.out.print("* ");
                 }
-
+                for (int j = 1; j <= i - 1; j++) {
+                    System.out.print("* ");
+                }
                 System.out.println();
+
             }
         }
     }
+
 }
