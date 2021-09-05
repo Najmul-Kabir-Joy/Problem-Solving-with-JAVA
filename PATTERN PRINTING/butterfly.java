3e90cfff1d3ddeb25519c9ab2b0c8f8bd81c75c9
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-09-05 11:17:49 +0600

    added butterfly

--- PATTERN PRINTING/butterfly.java
+++ PATTERN PRINTING/butterfly.java
@@ -5,6 +5,29 @@ public class butterfly {
         try (Scanner input = new Scanner(System.in)) {
             System.out.print("Enter pattern size: ");
             int n = input.nextInt();
+            int spaces = 2 * n - 1;
+            int stars = 0;
+            for (int i = 1; i <= 2 * n - 1; i++) {
+                if (i <= n) {
+                    spaces -= 2;
+                    stars++;
+                } else {
+                    spaces += 2;
+                    stars--;
+                }
+                for (int j = 1; j <= stars; j++) {
+                    System.out.print('*');
+                }
+                for (int j = 1; j <= spaces; j++) {
+                    System.out.print(' ');
+                }
+                for (int j = 1; j <= stars; j++) {
+                    if (j != n) {
+                        System.out.print('*');
+                    }
+                }
+                System.out.println();
+            }
         }
     }
 }
